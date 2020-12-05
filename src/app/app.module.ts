b3c92440b098dbf43db5da83bace8fd1a38bf804
Diego Alor <diegoalor@upeu.edu.pe>
2020-12-04 22:45:52 -0500

    Frontend

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FooterComponent } from './components/template/footer/footer.component';
+import { MenuComponent } from './components/template/menu/menu.component';
+import { LoginComponent } from './components/login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FooterComponent,
+    MenuComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
