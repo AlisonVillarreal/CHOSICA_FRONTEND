6806036e5ec6cb495af229811989c92480283220
alisonvillarreal <alisonvillarreal@upeu.edu.pe>
2020-12-08 01:29:02 -0500

    guardado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './components/template/footer/footer.component';
 import { MenuComponent } from './components/template/menu/menu.component';
 import { LoginComponent } from './components/login/login.component';
+import { OrganoComponent } from './components/organo/organo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     MenuComponent,
-    LoginComponent
+    LoginComponent,
+    OrganoComponent
   ],
   imports: [
     BrowserModule,
