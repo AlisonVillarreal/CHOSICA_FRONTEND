1bbf9929ab6efb1e2f68337ec9ac97e427e4232e
Diego Alor <diegoalor@upeu.edu.pe>
2020-12-10 23:52:34 -0500

    correcciones

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-<<<<<<< HEAD
-import { AppComponent } from './app.component';
 import { MenuComponent } from './components/template/menu/menu.component';
-
+import { InformeComponent } from './components/informe/informe.component';
+import { SolicitudRegistroComponent } from './components/solicitud-registro/solicitud-registro.component'
+import { OrganoComponent } from './components/organo/organo.component'
 import { AuthGuard } from './_helpers/auth.guard';
 
 const accountModule = () => import('./account/account.module').then(x => x.AccountModule);
@@ -11,23 +11,14 @@ const usersModule = () => import('./users/users.module').then(x => x.UsersModule
 
 const routes: Routes = [
 
-  { path: '', component: MenuComponent, canActivate: [AuthGuard] },
-  { path: 'users', loadChildren: usersModule, canActivate: [AuthGuard] },
-  { path: 'account', loadChildren: accountModule },
-
-  { path: '**', redirectTo: '' }
-=======
-import { InformeComponent } from './components/informe/informe.component';
-import { SolicitudRegistroComponent } from './components/solicitud-registro/solicitud-registro.component'
-import { OrganoComponent } from './components/organo/organo.component'
-
-const routes: Routes = [
   {path: 'informe', component: InformeComponent},
   {path: 'solicitud-registro', component: SolicitudRegistroComponent},
-  {path: 'organo', component: OrganoComponent }
->>>>>>> 3eb959f1f38fe092a3959585da857fb2e1d853e3
+  {path: 'organo', component: OrganoComponent },
+  {path: '', component: MenuComponent, canActivate: [AuthGuard] },
+  { path: 'users', loadChildren: usersModule, canActivate: [AuthGuard] },
+  { path: 'account', loadChildren: accountModule },
+  { path: '**', redirectTo: '' }
 ];
-
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,48 +1,24 @@
-import { NgModule } from '@angular/core';
-<<<<<<< HEAD
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule, FormsModule  } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-=======
+import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
->>>>>>> 3eb959f1f38fe092a3959585da857fb2e1d853e3
-
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { JwtInterceptor } from './_helpers/jwt.interceptor';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './components/template/footer/footer.component';
-import { JwtInterceptor } from './_helpers/jwt.interceptor';
 import { MenuComponent } from './components/template/menu/menu.component';
-<<<<<<< HEAD
-=======
-import { LoginComponent } from './components/login/login.component';
+import { LoginComponent } from '../app/account/login/login.component';
 import { OrganoComponent } from './components/organo/organo.component';
 import { InformeComponent } from './components/informe/informe.component';
 import { SolicitudRegistroComponent } from './components/solicitud-registro/solicitud-registro.component';
 import { CargoService } from './services/cargo.service';
 import { OrganizacionPersonaService } from './services/organizacion-persona.service';
 import { PersonaService } from './services/persona.service';
->>>>>>> 3eb959f1f38fe092a3959585da857fb2e1d853e3
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    ReactiveFormsModule,
-    FormsModule
-  ],
   declarations: [
     AppComponent,
     FooterComponent,
-<<<<<<< HEAD
-    MenuComponent
-  ],
-  
-  providers: [
-   {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi:true}
-  ],
-=======
     MenuComponent,
     LoginComponent,
     OrganoComponent,
@@ -56,10 +32,10 @@ import { PersonaService } from './services/persona.service';
     ReactiveFormsModule,
     HttpClientModule
   ],
-  providers: [CargoService,
+  providers: [{provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi:true},
+              CargoService,
               OrganizacionPersonaService,
               PersonaService],
->>>>>>> 3eb959f1f38fe092a3959585da857fb2e1d853e3
   bootstrap: [AppComponent]
 })
 export class AppModule { }
