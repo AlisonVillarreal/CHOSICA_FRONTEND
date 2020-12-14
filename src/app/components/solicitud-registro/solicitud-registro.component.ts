0b79990e67d34884dc08848c826c285b45d1958b
Diego Alor <diegoalor@upeu.edu.pe>
2020-12-14 03:06:46 -0500

    Tumbando Sistemas

--- src/app/components/solicitud-registro/solicitud-registro.component.ts
+++ src/app/components/solicitud-registro/solicitud-registro.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Solicitud_Fucion } from 'src/app/models/solicitud-organizacion';
+import { SolicitudOrganizacionService } from 'src/app/services/solicitud-organizacion.service';
 import { TipodenominacionService } from 'src/app/services/tipodenominacion.service';
 import { TipoorganizacionService } from 'src/app/services/tipoorganizacion.service';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-solicitud-registro',
@@ -12,9 +15,11 @@ export class SolicitudRegistroComponent implements OnInit {
 
   tiporganizacion : any;
   tipodenominacion: any;
+  solicitudModel: Solicitud_Fucion = new Solicitud_Fucion();
 
   constructor(private tipoorganizacionService: TipoorganizacionService,
               private tipodenominacionService: TipodenominacionService,
+              private solicitudOrganizacion: SolicitudOrganizacionService,
               private router: Router) { }
 
   ngOnInit(): void {
@@ -40,6 +45,18 @@ export class SolicitudRegistroComponent implements OnInit {
         this.tipodenominacion = datos[0]['CUR_TIPO_DENOMINACION'];
       },(err)=>{
         console.log("Error en el listar Tipo Denominaciones")
+      }
+    )
+  }
+
+  agregarSolicitud(): void{
+    this.solicitudOrganizacion.addSolicitudOrganizacion(this.solicitudModel).subscribe(
+      response=>{
+        swal.fire({
+          icon: 'success',
+          title: 'Solicitud Enviada',
+          text: 'Tu Solicitud de Organización a sido enviada correctamente'
+        })
       }
     )
   }
