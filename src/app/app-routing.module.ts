16c0383986d484f6fb0b5f5512d891fdfce1ea42
leonardo-mato-s <leonardomatos@upeu.edu.pe>
2020-12-14 10:49:37 -0500

    avance

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { InformeComponent } from './components/informe/informe.component';
 import { SolicitudRegistroComponent } from './components/solicitud-registro/solicitud-registro.component'
+import { RevisarSolicitudComponent } from './components/revisar-solicitud/revisar-solicitud.component';
+
 
 const routes: Routes = [
   {path: 'informe', component: InformeComponent},
-  {path: 'solicitud-registro', component: SolicitudRegistroComponent}
+  {path: 'solicitud-registro', component: SolicitudRegistroComponent},
+  {path: 'revisar-solicitud', component: RevisarSolicitudComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { LoginComponent } from './components/login/login.component';
 import { OrganoComponent } from './components/organo/organo.component';
 import { InformeComponent } from './components/informe/informe.component';
 import { SolicitudRegistroComponent } from './components/solicitud-registro/solicitud-registro.component';
+import { RevisarSolicitudComponent } from './components/revisar-solicitud/revisar-solicitud.component';
 
 
 @NgModule({
@@ -17,13 +18,13 @@ import { SolicitudRegistroComponent } from './components/solicitud-registro/soli
     FooterComponent,
     MenuComponent,
     LoginComponent,
-<<<<<<< HEAD
+
     OrganoComponent,
-    InformeComponent
-=======
     InformeComponent,
-    SolicitudRegistroComponent
->>>>>>> 0c193079a35cee6be1ce858f475590ab6574bf79
+    InformeComponent,
+    SolicitudRegistroComponent,
+    RevisarSolicitudComponent
+
   ],
   imports: [
     BrowserModule,
