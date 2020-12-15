0eaa860c41e35b125081f8870a00b55332109328
alisonvillarreal <alisonvillarreal@upeu.edu.pe>
2020-12-15 03:06:28 -0500

    alegrialison

--- src/app/components/organo/organo.component.ts
+++ src/app/components/organo/organo.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { from } from 'rxjs';
 import { Cargo } from 'src/app/models/cargo';
 import { OrganizacionPersona } from 'src/app/models/organizacion-persona';
 import { CargoService } from 'src/app/services/cargo.service';
 import { OrganizacionPersonaService } from 'src/app/services/organizacion-persona.service';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-organo',
@@ -11,11 +13,12 @@ import { OrganizacionPersonaService } from 'src/app/services/organizacion-person
   styleUrls: ['./organo.component.css']
 })
 export class OrganoComponent implements OnInit {
-
+  listapersonas: any;
   organo: any;
   organoModel: OrganizacionPersona = new OrganizacionPersona();
   cargo: any;
   cargoModel: Cargo = new Cargo();
+  numero_doc: String; id_cargo: number;
 
   constructor(private cargoservice: CargoService,
               private organoservice: OrganizacionPersonaService,
@@ -23,6 +26,7 @@ export class OrganoComponent implements OnInit {
 
   ngOnInit(): void {
     this.listarcargos();
+    this.listar();
   }
 
   listarcargos(): void{
@@ -33,6 +37,35 @@ export class OrganoComponent implements OnInit {
       },(err)=>{
         console.log("Error en el Organo listar")
       }
+    )
+  }
+  listardocumento(documento: String): void{
+    this.organoservice.getBuscarDocu(documento).subscribe(
+      (datos)=>{ console.log(datos)
+        console.log(datos['CUR_PERSONA'])
+      }
+    )
+  }
+  listar(): void{
+    this.organoservice.getLista().subscribe(
+      (datos)=>{ console.log(datos)
+        this.listapersonas= datos[0]['CUR_PERSONA']
+        console.log(datos[0]['CUR_PERSONA'])
+      }
+    )
+  }
+  updCargo(): void{
+    var numero_doc = this.numero_doc;
+    var id_cargo = this.id_cargo;
+    console.log(numero_doc, id_cargo)
+    this.organoservice.updCargo(numero_doc, id_cargo).subscribe(
+      (datos)=>{ 
+        this.listar();
+      swal.fire({
+        icon: 'success',
+        title: 'Solicitud Enviada',
+        text: 'Tu Solicitud de Organización a sido enviada correctamente'
+      })}
     )
   }
 }
